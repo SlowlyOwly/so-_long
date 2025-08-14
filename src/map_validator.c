a2ae838f6090b87dfe2bd9714d17cdb96e0ede56
Sebastian Rogozinski <srogozin@c3r11s1.42warsaw.pl>
2025-08-14 20:24:01 +0200

    map validator

--- src/map_validator.c
+++ src/map_validator.c
@@ -6,7 +6,7 @@
 /*   By: srogozin <srogozin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 17:35:42 by srogozin          #+#    #+#             */
-/*   Updated: 2025/08/14 20:00:38 by srogozin         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:23:28 by srogozin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,18 @@ int	map_shape_checker(char **map_data)
 int	map_wall_checker(char **map_data)
 {
 	int	i;
+	int	line_num;
 
-	
+	i = 0;
+	line_num = 
+	while (map_data[0][i] != '\0' || map_data[line_num - 1][i] != '\0')
+	{
+		if (map_data[0][i] != '1' || map_data[line_num - 1][i] != '\0')
+		{
+			ft_printf("Error\nMap has break in wall.\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
